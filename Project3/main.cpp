f255f9e04191232f575b771989e3f086bf783620
Jared <jared.wood002@gmail.com>
2022-09-23 12:13:21 -0500

    Created Basic Class, to set and get numbers, in addition default construction added.

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 
 using namespace std;
+
+class myInt
+{
+    private:
+        int userNumber;
+    public:
+        int getInt()
+        {
+            return userNumber;
+        }
+        void setInt(int num)
+        {
+            userNumber = num;
+        }
+        bool isMultiple(int multiplier);
+        bool sumIsEven();
+        double getSquareRoot();
+        bool isPrime();
+        bool isPerfect();
+        myInt()
+        {
+            userNumber = 0;
+        }
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    myInt num1;
+    num1.setInt(5);
+    cout << num1.getInt() << endl;
+
+    myInt num2;
+    cout << num2.getInt() << endl;
     return 0;
 }
