0fe3d497c4c2017787646469645f09979522eea0
Jared <jared.wood002@gmail.com>
2022-09-23 12:05:29 -0500

    Initial Commit: Project 3 Begun

---
+++ Project3/cmake-build-debug/CMakeFiles/DataStructuresAndAlgorithms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructuresAndAlgorithms.dir/main.cpp.obj"
+  "DataStructuresAndAlgorithms.exe"
+  "DataStructuresAndAlgorithms.exe.manifest"
+  "DataStructuresAndAlgorithms.lib"
+  "DataStructuresAndAlgorithms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructuresAndAlgorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
