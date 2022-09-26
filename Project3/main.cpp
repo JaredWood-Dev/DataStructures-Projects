e4a539d29d6691f033fb9b00916236cdc5ec5b02
Jared <jared.wood002@gmail.com>
2022-09-26 12:41:45 -0500

    Changed functions to accuratley use function prototyping.

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -8,76 +8,14 @@ class myInt
     private:
         int userNumber;
     public:
-        int getInt()
-        {
-            return userNumber;
-        }
-        void setInt(int num)
-        {
-            userNumber = num;
-        }
-        bool isMultiple(int multiplier)
-        {
-            if (userNumber % multiplier == 0)
-            {
-                return true;
-            }
-            return false;
-        }
-        bool sumIsEven()
-        {
-            int sumNum = 0;
-            int tempNum = userNumber;
-            while (tempNum > 0)
-            {
-                int digit = tempNum % 10;
-                tempNum = tempNum / 10;
-                sumNum = sumNum + digit;
-            }
-            if (sumNum % 2 == 0)
-            {
-                return true;
-            }
-            return false;
-        }
-        double getSquareRoot()
-        {
-            return pow(userNumber, 0.5);
-        }
-        bool isPrime()
-        {
-            int multCount = 0;
-            for (int i = 1; i <= userNumber; i++) {
-                if (userNumber % i == 0)
-                {
-                    multCount++;
-                }
-            }
-            if (multCount > 2)
-            {
-                return false;
-            }
-            return true;
-        }
-        bool isPerfect()
-        {
-            int perfectCount = 0;
-            for (int i = 1; i < userNumber; ++i) {
-                if (userNumber % i == 0)
-                {
-                    perfectCount = perfectCount + i;
-                }
-            }
-            if (perfectCount == userNumber)
-            {
-                return true;
-            }
-            return false;
-        }
-        myInt()
-        {
-            userNumber = 0;
-        }
+        int getInt();
+        void setInt(int num);
+        bool isMultiple(int multiplier);
+        bool sumIsEven();
+        double getSquareRoot();
+        bool isPrime();
+        bool isPerfect();
+        myInt();
 };
 int main() {
 
@@ -92,4 +30,74 @@ int main() {
     cout << "Is Prime: " << num1.isPrime() << endl;
     cout << "Is Perfect: " << num1.isPerfect() << endl;
     return 0;
+}
+int myInt::getInt()
+{
+    return userNumber;
+}
+void myInt::setInt(int num)
+{
+    userNumber = num;
+}
+bool myInt::isMultiple(int multiplier)
+{
+    if (userNumber % multiplier == 0)
+    {
+        return true;
+    }
+    return false;
+}
+bool myInt::sumIsEven()
+{
+    int sumNum = 0;
+    int tempNum = userNumber;
+    while (tempNum > 0)
+    {
+        int digit = tempNum % 10;
+        tempNum = tempNum / 10;
+        sumNum = sumNum + digit;
+    }
+    if (sumNum % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+double myInt::getSquareRoot()
+{
+    return pow(userNumber, 0.5);
+}
+bool myInt::isPrime()
+{
+    int multCount = 0;
+    for (int i = 1; i <= userNumber; i++) {
+        if (userNumber % i == 0)
+        {
+            multCount++;
+        }
+    }
+    if (multCount > 2)
+    {
+        return false;
+    }
+    return true;
+}
+bool myInt::isPerfect()
+{
+    int perfectCount = 0;
+    for (int i = 1; i < userNumber; ++i) {
+        if (userNumber % i == 0)
+        {
+            perfectCount = perfectCount + i;
+        }
+    }
+    if (perfectCount == userNumber)
+    {
+        return true;
+    }
+    return false;
+}
+myInt::myInt()
+{
+    userNumber = 0;
 }
