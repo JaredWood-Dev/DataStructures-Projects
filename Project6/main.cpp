0e3aa19c71da0cf6a6055e7eaf1f2f6a5db26f19
Jared <jared.wood002@gmail.com>
2022-11-07 10:24:08 -0600

    Project 6 Started

---
+++ Project6/main.cpp
@@ -0,0 +1,14 @@
+//Author: Jared Wood
+//Due Date: 11/9/22
+//Programming Assignment
+//Fall 2022 - CS 3358 - 001
+//Instructor: Husain Gholoom
+//Description:
+//To demonstrate understanding and implementation of sort and search algorithms
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
