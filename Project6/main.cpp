bc91653839f461965834adf2f5a9988ea5e434f3
Jared <jared.wood002@gmail.com>
2022-11-08 13:37:01 -0600

    Completed display() functions.

--- Project6/main.cpp
+++ Project6/main.cpp
@@ -9,6 +9,12 @@
 #include <iostream>
 #include <ctime>
 
+void display(int input[], int length);
+//Displays up to 20 elements up the int array
+
+void display(char input[], int length);
+//Displays up to 20 elements of the char array
+
 using namespace std;
 int main() {
 
@@ -39,7 +45,9 @@ int main() {
         intArray[i] = rand() % 25 + 1;
     }
 
-    
+    display(charArray, userNum);
+    display(intArray, userNum);
+
 
     //Footer
     cout << "November 2022" << endl;
@@ -47,3 +55,49 @@ int main() {
     cout << "Jared Wood" << endl;
     return 0;
 }
+
+void display(int input[], int length)
+{
+
+    //Check if it is greater than 20
+    if (length > 20)
+    {
+        length = 20;
+    }
+
+    cout << "Integer array elements are: ";
+    //Loop through up to the first 20 elements, and display them
+    for (int i = 0; i < length; ++i) {
+        cout << input[i] << " ";
+    }
+    cout << endl;
+}
+//-----------------------------------------------------------------------------
+//Displays the first 20 elements of the array.
+//
+//param1: input - The array to be displayed
+//param2: length - The length of the array
+//-----------------------------------------------------------------------------
+
+void display(char input[], int length)
+{
+
+    //Check if it is greater than 20
+    if (length > 20)
+    {
+        length = 20;
+    }
+
+    cout << "Character Array elements are: ";
+    //Loop through up to the first 20 elements, and display them
+    for (int i = 0; i < length; ++i) {
+        cout << input[i] << " ";
+    }
+    cout << endl;
+}
+//-----------------------------------------------------------------------------
+//Displays the first 20 elements of the array.
+//
+//param1: input - The array to be displayed
+//param2: length - The length of the array
+//-----------------------------------------------------------------------------
